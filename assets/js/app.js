7d13f64c8e8558799ce496b603df99d1c36caff5
zaherh8 <hassanzaher11@gmail.com>
2024-06-03 18:24:43 +0300

    countdown added & design fixes

--- assets/js/app.js
+++ assets/js/app.js
@@ -34,6 +34,32 @@ topbar.config({barColors: {0: "#29d"}, shadowColor: "rgba(0, 0, 0, .3)"})
 window.addEventListener("phx:page-loading-start", info => topbar.show())
 window.addEventListener("phx:page-loading-stop", info => topbar.hide())
 
+// assets/js/app.js
+
+document.addEventListener('DOMContentLoaded', () => {
+    const countdownElement = document.getElementById('countdown');
+    const targetDate = new Date('July 7, 2024 00:00:00').getTime();
+  
+    function updateCountdown() {
+      const now = new Date().getTime();
+      const distance = targetDate - now;
+  
+      const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  
+      countdownElement.innerHTML = `${days}d ${hours}h ${minutes}m ${seconds}s`;
+      if (distance < 0) {
+        countdownElement.classList.add("hidden");
+        const countdownElement = document.getElementById('countdown');
+      }
+    }
+  
+    updateCountdown();
+    const interval = setInterval(updateCountdown, 1000);
+  });
+  
 // connect if there are any LiveViews on the page
 liveSocket.connect()
 
